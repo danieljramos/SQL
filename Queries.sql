4ab2a3decf17b3db36387329d012cc4e0d0e64fe
Daniel Ramos <114722372+danieljramos@users.noreply.github.com>
2023-10-30 13:07:49 +0100

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,63 +1,134 @@
 -- 01 How would you use a subquery to find the total number of orders placed by customers who have made a purchase more than once?
 
-Select 
-sum(salary) from 
-(select emp_no, count(salary) as counter, sum(salary) as salary from salaries group by emp_no) as count where counter > 2
+SELECT sum(salary)
+FROM
+  (SELECT emp_no,
+          count(salary) AS counter,
+          sum(salary) AS salary
+   FROM salaries
+   GROUP BY emp_no) AS COUNT
+WHERE counter > 2
 
 -- using with clause -- 
 
-with count as (select emp_no, count(salary) as counter from salaries group by emp_no)
-
-select
-sum(salary) from salaries left join count on salaries.emp_no = count.emp_no
-where
-counter > 2
+WITH COUNT AS
+  (SELECT emp_no,
+          count(salary) AS counter
+   FROM salaries
+   GROUP BY emp_no)
+SELECT sum(salary)
+FROM salaries
+LEFT JOIN COUNT ON salaries.emp_no = count.emp_no
+WHERE counter > 2
 
 -- 02 Write a query to identify products that have been ordered more times than the average number of orders per product.
 
-select product  from (select product, count(orders) as count_order from product) as total_orders where count_order > (select avg(count_order) from total_orders)
+SELECT product
+FROM
+  (SELECT product,
+          count(orders) AS count_order
+   FROM product) AS total_orders
+WHERE count_order >
+    (SELECT avg(count_order)
+     FROM total_orders)
 
 -- Using with clause --  
   
-with orders as (select product , count(orders) as order_count from products)
-select product from orders where order_count > avg(sum(orders)) or WHERE order_count > (SELECT AVG(order_count) FROM orders)
+  WITH orders AS
+  (SELECT product,
+          count(orders) AS order_count
+   FROM products)
+SELECT product
+FROM orders
+WHERE order_count > avg(sum(orders))
+  OR WHERE order_count >
+    (SELECT AVG(order_count)
+     FROM orders)
 
-with orders as (select product , count(orders) as order_count from products)
-select product from orders where order_count > avg(sum(orders)) or WHERE order_count > (SELECT AVG(order_count) FROM orders)
 
 -- 03 How would you find employees who joined the company after the most recent hire date within their department using a subquery?
-select last_name from employees  where hire_date >= (select max(hire_date) from employees)
+
+  SELECT last_name
+FROM employees
+WHERE hire_date >=
+    (SELECT max(hire_date)
+     FROM employees)
 
 -- 04 Write a query to retrieve customers who have placed orders worth more than the average order amount.
 
-select emp_no from salaries group by emp_no having sum(salary)> (select avg(salary) from salaries)
+SELECT emp_no
+FROM salaries
+GROUP BY emp_no
+HAVING sum(salary)>
+  (SELECT avg(salary)
+   FROM salaries)
 
 -- 05 Write a query to display product details along with the percentage difference between the product's price and the average price of all products.'
 
-select a.emp_no, avg(a.avg_salary)/sum(a.t), sum(a.avg_salary)/sum(a.t)
-from
-(select emp_no,(select avg(salary) from salaries) as avg_salary , sum(salary) as t from salaries group by emp_no, avg_salary ) a group by emp_no
+SELECT a.emp_no,
+       avg(a.avg_salary)/sum(a.t),
+       sum(a.avg_salary)/sum(a.t)
+FROM
+  (SELECT emp_no,
 
--- Using with clause --
-with avg_salary as (select emp_no, avg(salary) as avg_salary from salaries group by emp_no)
-select salaries.emp_no, avg(avg_salary)/sum(salary)  from salaries left join avg_salary on   salaries.emp_no = avg_salary.emp_no group by salaries.emp_no
-
--- 06 Write a query to find employees whose manager has a higher salary than the average salary of all managers.
-
-Select emp_no as employee from salaries where 
-
--- 07 How would you use a window function to rank emp by their total salary within each salary employee?
-
-select emp_no, rank() over(order by total_salary desc) from (select  emp_no, sum(salary) as total_salary from salaries group by emp_no) as total
-
--- 08 get top 5
-select emp_no from (select emp_no, rank() over(order by salary desc) as ranking from salaries) as ranking where ranking < 5
+     (SELECT avg(salary)
+      FROM salaries) AS avg_salary,
+          sum(salary) AS t
+   FROM salaries
+   GROUP BY emp_no,
+            avg_salary) a
+GROUP BY emp_no
 
 -- Using with clause --
   
-with total_salary as (select emp_no, sum(salary) as total_salary from salaries group by emp_no),  
-ranking as ( select emp_no, rank() over(order by total_salary desc) as ranking1 from total_salary)
-select emp_no, ranking1 from ranking where ranking1 < 5
+WITH avg_salary AS
+  (SELECT emp_no,
+          avg(salary) AS avg_salary
+   FROM salaries
+   GROUP BY emp_no)
+SELECT salaries.emp_no,
+       avg(avg_salary)/sum(salary)
+FROM salaries
+LEFT JOIN avg_salary ON salaries.emp_no = avg_salary.emp_no
+GROUP BY salaries.emp_no
+
+-- 06 How would you use a window function to rank emp by their total salary within each salary employee?
+
+SELECT emp_no,
+       rank() over(
+                   ORDER BY total_salary DESC)
+FROM
+  (SELECT emp_no,
+          sum(salary) AS total_salary
+   FROM salaries
+   GROUP BY emp_no) AS total
+
+-- 07 get top 5
+  
+SELECT emp_no
+FROM
+  (SELECT emp_no,
+          rank() over(
+                      ORDER BY salary DESC) AS ranking
+   FROM salaries) AS ranking
+WHERE ranking < 5
+
+-- Using with clause --
+  
+WITH total_salary AS
+  (SELECT emp_no,
+          sum(salary) AS total_salary
+   FROM salaries
+   GROUP BY emp_no),
+     ranking AS
+  (SELECT emp_no,
+          rank() over(
+                      ORDER BY total_salary DESC) AS ranking1
+   FROM total_salary)
+SELECT emp_no,
+       ranking1
+FROM ranking
+WHERE ranking1 < 5
 
 
 
